90e02d9727b81bd72b8c9b9bede8497345d44f0d
Brian Cardarella <brian@dockyard.com>
2016-03-03 17:11:30 -0500

    Version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule AuthTestSupport.Mixfile do
 
   def project do
     [app: :auth_test_support,
-     version: "0.0.5",
+     version: "0.0.6",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
